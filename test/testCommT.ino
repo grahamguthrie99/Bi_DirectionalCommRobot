9e899a37a19810cb7ba9746a9fd05515c6635195
Graham Guthrie <grahamguthrie99@gmail.com>
2018-02-14 18:49:21 -0500

    Update testCommT.ino
    beginning controller code. operates in three states: on/off -> on -> pan/tilt -> off

--- test/testCommT.ino
+++ test/testCommT.ino
@@ -6,12 +6,21 @@
 
 
 #define button 2
-
+#define bLight 3
+#define gLight 4
+#define rLight 5
+#define touchSense 6
 
 RF24 radio(7, 8); // CE, CSN
+
 const byte address[6] = "00001";
 boolean buttonState = 0;
 boolean lastButtonState = 0;
+boolean currentState =0;
+boolean lastState = 0;
+boolean onOff = 0;
+boolean machState = 0;
+
 
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;
@@ -19,8 +28,10 @@ unsigned long debounceDelay = 50;
 void setup() {
   //Button
   pinMode(button, INPUT);
-  pinMode(4, OUTPUT);
-  
+  pinMode(touchSense, INPUT);
+  pinMode(rLight, OUTPUT);
+  pinMode(bLight, OUTPUT);
+  pinMode(gLight, OUTPUT);
   Serial.begin(9600);
   //COMM
   radio.begin();
@@ -30,14 +41,70 @@ void setup() {
 }
 
 void loop() {
-  buttonState = digitalRead(button);
-  if (buttonState != lastButtonState) {
+  /////////////////////////////////
+  
+  boolean buttonReading = digitalRead(button);
+  turnON(buttonReading);
+  radio.write(&onOff, sizeof(onOff));
+  if(onOff)
+  {
+    radio.write(&machState, sizeof(machState));
+    boolean stateReading =digitalRead(touchSense);
+    debounceState(stateReading);
+    if(machState ==1){
+        color(0,255, 0); 
+        Serial.print("MOVE");
+    }
+    else{
+        color(0,0,255);
+        Serial.print("PAN");
+    }
+    
+  }
+  else{
+    color(255, 0, 0);
+    machState = 0;
+  }
+  
+}
+////////////////////////////////
+////////////////////////////////
+void turnON(boolean reading){
+  if (reading != lastButtonState) {
       lastDebounceTime = millis();
   }
-    if ((millis() - lastDebounceTime) > debounceDelay) {
-      if (buttonState != lastButtonState) {
-        buttonState = lastButtonState;
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+      if (reading != buttonState) {
+         buttonState = reading;
+          if(buttonState == 1){
+            onOff = !(onOff);
+          }
       }
+  }
+  lastButtonState = reading; 
+}
+////////////////////////////////
+void debounceState(boolean reading){
+  if (reading != lastState) {
+      lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+      if (reading != currentState) {
+         currentState = reading;
+          if(currentState == 1){
+            machState = !(machState);
+          }
+      }
+  }
+  lastState = reading; 
+}
+////////////////////////////////
+void color (unsigned char red, unsigned char green, unsigned char blue) // the color generating function 
+{ 
+  analogWrite(rLight, red); 
+  analogWrite(bLight, blue); 
+  analogWrite(gLight, green); 
+}
     }
   digitalWrite(4, buttonState);
   Serial.print(buttonState);
